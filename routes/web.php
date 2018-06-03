22e9a4d80b172b09a8ec64c807a5781e8bc0ddfa
Ilya - Appstockus.com <ilya@appstockus.com>
2018-06-03 23:06:15 +0300

    Продолжаем 1-й урок

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
