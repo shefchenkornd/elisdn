a36413c996b4d709459116170ec1869c72ff8cbb
Ilya - Appstockus <ilya@appstockus.com>
2018-06-27 13:33:20 +0300

    Подправил инструкцию 1.txt: как поднять node+npm+laravel-mix

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-25 05:49:36.
+    * Generated on 2018-06-27 08:59:53.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
