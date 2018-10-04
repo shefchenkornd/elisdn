c2deeb33347020230040b3939908918c31ad234e
Ilya - Appstockus.com <ilya@appstockus.com>
2018-10-04 10:39:33 +0300

    Добавил breadcrums для Admin\User

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -27,7 +27,70 @@ Breadcrumbs::register('password.reset', function ($breadcrumbs) {
     $breadcrumbs->push('Change', route('password.reset'));
 });
 
+/**
+ * Cabinet
+ */
 Breadcrumbs::register('cabinet', function ($breadcrumbs){
     $breadcrumbs->parent('home');
     $breadcrumbs->push('Cabinet', route('cabinet'));
 });
+
+/**
+ * Admin User
+ */
+Breadcrumbs::register('admin.users.index', function ($breadcrumbs){
+    $breadcrumbs->parent('admin.home');
+    $breadcrumbs->push('Users', route('admin.users.index'));
+});
+
+Breadcrumbs::register('admin.users.create', function ($breadcrumbs){
+    $breadcrumbs->parent('admin.users.index');
+    $breadcrumbs->push('Create', route('admin.users.create'));
+});
+
+
+Breadcrumbs::register('admin.users.show', function ($breadcrumbs, User $user){
+    $breadcrumbs->parent('admin.users.index');
+    $breadcrumbs->push($user->name, route('admin.users.show', $user));
+});
+
+Breadcrumbs::register('admin.users.edit', function ($breadcrumbs, User $user){
+    $breadcrumbs->parent('admin.users.index', $user);
+    $breadcrumbs->push('Edit', route('admin.users.edit', $user));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
