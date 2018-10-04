af2fd7fc96db12291abf4093b05f6c4147906f61
Ilya - Appstockus.com <ilya@appstockus.com>
2018-10-04 10:29:04 +0300

    Переместил и обновил User model

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -1,13 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Entity;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @proprerty int $id
+ * @proprerty string $name
+ * @proprerty string $email
+ * @proprerty string $status
+ */
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public const STATUS_ACTIVE = 'active';
+    public const STATUS_WAIT = 'wait';
 
     /**
      * The attributes that are mass assignable.
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilya
+ * Date: 01.10.18
+ * Time: 9:37
+ */
