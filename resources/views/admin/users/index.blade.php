37bc1b15b439e64930aa4250a7acf6b423c771d3
Ilya - Appstockus.com <ilya@appstockus.com>
2019-02-13 14:11:23 +0300

    добавил фильтр для user в админке

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,6 +9,85 @@
 
     <br><br>
 
+
+    <div class="card mb-3">
+        <div class="card-header">Filter</div>
+        <div class="card-body">
+            <form action="?" method="GET">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <div class="form-group">
+                            <label for="id" class="col-form-label">ID</label>
+                            <input name="id" id="id" type="text" class="form-control" value="{{ request('id') }}">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="name" class="col-form-label">Name</label>
+                            <input name="name" id="name" type="text" class="form-control" value="{{ request('name') }}">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="email" class="col-form-label">Email</label>
+                            <input name="email" id="email" type="text" class="form-control" value="{{ request('email') }}">
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="status" class="col-form-label">Status</label>
+                            <select name="status" id="status" class="form-control">
+                                <option value=""></option>
+                                @foreach($statuses as $value => $label)
+                                    <option value="{{ $value }}"  {{ $value === request('status') ? 'selected' : '' }}>
+                                        {{ $label }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="role" class="col-form-label">Role</label>
+                            <select name="role" id="role" class="form-control">
+                                <option value=""></option>
+                                @foreach($roles as $value => $label)
+                                    <option value="{{ $value }}"  {{ $value === request('role') ? 'selected' : '' }}>
+                                        {{ $label }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
@@ -16,6 +95,7 @@
             <th>Name</th>
             <th>Email</th>
             <th>Status</th>
+            <th>Role</th>
         </tr>
         </thead>
         <tbody>
